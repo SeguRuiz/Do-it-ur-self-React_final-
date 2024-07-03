3365c499bd3021a31490aeb0fd7cf3c894e1e3ff
SeguRuiz <ruizsegura96199@gmail.com>
2024-07-03 15:16:30 -0600

    estilizando

--- src/container/components/Filter/showFilter.jsx
+++ src/container/components/Filter/showFilter.jsx
@@ -1,31 +1,33 @@
-import { useRef, useState,  } from "react"
-import { useTheContext} from "../../../context/ContextProvider"
-import { ProductPut_Tools } from "../../../utils/Fetchs/classes"
+import { useRef, useState } from "react";
+import { useTheContext } from "../../../context/ContextProvider";
+import { ProductPut_Tools } from "../../../utils/Fetchs/classes";
 const ShowFilter = () => {
-    const {tags, producstData, data, updateData, setActiveTag } = useTheContext()
-    const selectRef = useRef()
+  const { tags, producstData, data, updateData, setActiveTag } =
+    useTheContext();
+  const selectRef = useRef();
 
-    const [value, setValue] = useState('hola')
-    
-    const changeSelect = (o) =>{
-      setValue(o.target.value)
-      setActiveTag(o.target.value)
-      setTimeout(()=>{
-        updateData(data + 1)
-      }, 200)
-    }
+  const [value, setValue] = useState("hola");
 
+  const changeSelect = (o) => {
+    setValue(o.target.value);
+    setActiveTag(o.target.value);
+    setTimeout(() => {
+      updateData(data + 1);
+    }, 200);
+  };
 
   return (
     <>
-    <select ref={selectRef} onChange={changeSelect} value={value}>
-    <option value={'defecto'}>defecto</option>
-   {tags.map((tags, i) => (
-    <option value={tags.tagName} key={i} id={tags.id}>{tags.tagName}</option>
-   ))}
-   </select>
-   </>
-  )
-}
+      <select ref={selectRef} onChange={changeSelect} value={value}>
+        <option value={"defecto"}>defecto</option>
+        {tags.map((tags, i) => (
+          <option value={tags.tagName} key={i} id={tags.id}>
+            {tags.tagName}
+          </option>
+        ))}
+      </select>
+    </>
+  );
+};
 
-export default ShowFilter
+export default ShowFilter;
