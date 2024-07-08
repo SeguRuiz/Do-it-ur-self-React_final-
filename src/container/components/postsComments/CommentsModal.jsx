349816e583f93829659de3c4a8214137dcd94367
SeguRuiz <ruizsegura96199@gmail.com>
2024-07-08 11:47:35 -0600

    Componente para agregar eliminar, y visualizar comentarios agregados.

--- src/container/components/postsComments/CommentsModal.jsx
+++ src/container/components/postsComments/CommentsModal.jsx
@@ -1,9 +1,11 @@
 import { useRef } from "react";
 import AddComments from "./AddComments";
+import ShowComments from "./ShowComments";
 
 const CommentsModal = ({ post_id }) => {
+ 
   const modalDlg = useRef();
-
+ 
   const openModal = () => {
     modalDlg.current.showModal();
   };
@@ -21,6 +23,7 @@ const CommentsModal = ({ post_id }) => {
           <button onClick={close}>close</button>
           <div>
             <h2>comments area</h2>
+            <ShowComments id={post_id}/>
           </div>
           <div>
             <AddComments id={post_id} closeModal={close}/>
